6fe6660297961bf9d0132fe65b61e9ba195eb5d8
jckanellis <jckanellis889@gmail.com>
2021-02-28 22:17:14 -0800

    Configure mailer for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -5,7 +5,7 @@ Rails.application.configure do
   config.cache_classes = true
 
 
-  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.default_url_options = { :host => 'https://profootballstats.herokuapp.com/' }
 
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
@@ -33,6 +33,17 @@ Rails.application.configure do
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.serve_static_assets = true
   config.assets.compile = true
+
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+     :address => "smtp.gmail.com",
+     :port => 587,
+     :user_name => "donotreplyfootballsite@gmail.com",
+     :password => "G0L@kers",
+     :authentication => :plain,
+     :enable_starttls_auto => true
+}
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
