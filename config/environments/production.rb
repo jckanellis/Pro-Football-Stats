0056057dfc78dbf819a6a39569f84344a11ff26f
jckanellis <jckanellis889@gmail.com>
2021-03-03 23:09:00 -0800

    Change SendGrid Port

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -39,7 +39,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     :address => "smtp.sendgrid.net",
     :domain => "profootballstats.app",
-    :port => 587,
+    :port => 465,
     :user_name => ENV['SENDGRID_USERNAME'],
     :password => ENV['SENDGRID_PASSWORD'],
     :authentication => :plain,
